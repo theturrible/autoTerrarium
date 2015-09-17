f6a19a23f09d07d196fd198b3c0a7f586bbbad2c
Ivan Grinkevich <grin.van@gmail.com>
2015-09-16 19:11:01 -0700

    adding another little debug

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ var htu21df = new i2c_htu21d();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
+	console.log("getting")
   	var locals = {};
     async.parallel([
         //Load user
@@ -36,7 +36,7 @@ router.get('/', function(req, res, next) {
     ], function(err) { //This function gets called after the two tasks have called their "task callbacks"
         if (err) return next(err); //If an error occured, we let express/connect handle it by calling the "next" function
         //Here locals will be populated with 'user' and 'posts'
-        res.render('index', { title: 'Hey', message: 'Hello there!', temp: locals.env.temperature, humid: locals.env.humidity);
+        res.render('index', { title: 'Hey', message: 'Hello there!', temp: locals.env.temperature, humid: locals.env.humidity});
     });
 
 	
