06284f1288e96be1ae17be844c1eef3113d6719c
Ivan Grinkevich <grin.van@gmail.com>
2015-09-16 19:17:33 -0700

    maybe?

--- routes/index.js
+++ routes/index.js
@@ -8,40 +8,27 @@ var async = require('async')
 // If using any other board, specify the device name.
 // For example: i2c_htu21d({device: '/dev/i2c-1/'});
 
+var htu21df = new i2c_htu21d();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var htu21df = new i2c_htu21d();
+	
 	console.log("getting")
   	var locals = {};
-    async.parallel([
-        //Load user
-        function(callback) {
-          htu21df.readTemperature(function (temp) {
-    			console.log('Temperature, C:', temp);
-                locals.env = {
-                    temperature: temp
-                };
-                callback();
-            });
-        },
-        function(callback) {
-          htu21df.readHumidity(function (humidity){
+  	htu21df.readTemperature(function (temp) {
+		console.log('Temperature, C:', temp);
+            locals.env = {
+                temperature: temp
+            };
+            htu21df.readHumidity(function (humidity){
     			console.log('humidity', humidity);
                 locals.env = {
                     humidity: humidity
                 };
-                callback();
-            });
-        }
-    ], function(err) { //This function gets called after the two tasks have called their "task callbacks"
-        if (err) return next(err); //If an error occured, we let express/connect handle it by calling the "next" function
-        //Here locals will be populated with 'user' and 'posts'
-        res.render('index', { title: 'Hey', message: 'Hello there!', temp: locals.env.temperature, humid: locals.env.humidity});
+	                res.render('index', { title: 'Hey', message: 'Hello there!', temp: locals.env.temperature, humid: locals.env.humidity});
+			});
     });
-
-	
-
+   
 });
 
 module.exports = router;
