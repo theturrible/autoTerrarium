43e623121d28e9cd778164980013c2ce462a4608
Ivan Grinkevich <grin.van@gmail.com>
2015-09-16 19:13:56 -0700

    maybe?

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,11 @@ var async = require('async')
 // Older boards use /dev/i2c-0, newer ones use /dev/i2c-1.
 // If using any other board, specify the device name.
 // For example: i2c_htu21d({device: '/dev/i2c-1/'});
-var htu21df = new i2c_htu21d();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	var htu21df = new i2c_htu21d();
 	console.log("getting")
   	var locals = {};
     async.parallel([
@@ -36,7 +37,7 @@ router.get('/', function(req, res, next) {
     ], function(err) { //This function gets called after the two tasks have called their "task callbacks"
         if (err) return next(err); //If an error occured, we let express/connect handle it by calling the "next" function
         //Here locals will be populated with 'user' and 'posts'
-        res.render('index', { title: 'Hey', message: 'Hello there!', temp: locals.env.temperature, humid: locals.env.humidity});
+        res.render('index', { title: 'Hey', message: 'Hello there!', temp: locals.env.temperature, humid: locals.env.humidity})cd .;
     });
 
 	
